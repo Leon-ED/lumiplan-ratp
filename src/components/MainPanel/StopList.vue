0a63df1d149974fb6a22fec55587d5efd86ab177
Leon-ED <edmeeleon@outlook.fr>
2025-11-26 01:23:06 +0100

    La ligne s'arrête maintenant au niveau du dernier arrêt

--- src/components/MainPanel/StopList.vue
+++ src/components/MainPanel/StopList.vue
@@ -1,29 +1,21 @@
 <script setup lang="ts">
 defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
 </script>
+
 <template>
   <div class="stops-list-container">
     <ol class="stops-list">
-      <!-- <li class="stop next-stop">
-        <div class="stop-indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 640 640"
-            class="arrow-icon"
-          >
-            <path
-              d="M297.4 566.6C309.9 579.1 330.2 579.1 342.7 566.6L502.7 406.6C515.2 394.1 515.2 373.8 502.7 361.3C490.2 348.8 469.9 348.8 457.4 361.3L352 466.7L352 96C352 78.3 337.7 64 320 64C302.3 64 288 78.3 288 96L288 466.7L182.6 361.3C170.1 348.8 149.8 348.8 137.3 361.3C124.8 373.8 124.8 394.1 137.3 406.6L297.3 566.6z"
-            />
-          </svg>
-        </div>
-        <span>Prochains arrêts</span>
-      </li> -->
       <TransitionGroup
         name="stop-transition"
         tag="div"
         class="stops-transition-wrapper"
       >
-        <li v-for="stop in stops.slice(0, 2)" :key="stop.stop.name" class="stop blinkable" :class="{'is-last-stop' : stop.isTerminus}">
+        <li
+          v-for="stop in stops.slice(0, 2)"
+          :key="stop.stop.name"
+          class="stop blinkable"
+          :class="{ 'is-last-stop': stop.isTerminus }"
+        >
           <div class="stop-indicator"></div>
           <span class="stop-name">{{ stop.stop.name }}</span>
         </li>
@@ -37,7 +29,7 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
   overflow: hidden;
   height: 100%;
   width: 100%;
-  background-color: var(--ratp-blue);
+  background-color: var(--ratp-blue); /* Fallback */
   font-family: "ParisineBold", sans-serif;
   display: flex;
   flex-direction: column;
@@ -54,10 +46,12 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
   position: relative;
   display: flex;
   flex-direction: column;
-  align-items: flex-start; 
+  align-items: flex-start;
   justify-content: space-around;
   padding-right: 2cqw;
 }
+
+/* La ligne infinie en arrière-plan */
 .stops-list::before {
   content: "";
   position: absolute;
@@ -78,16 +72,28 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
   position: relative;
 }
 
-.stop-indicator {
+.stop.is-last-stop::after {
+  content: "";
+  position: absolute;
+  left: 2cqw;
   width: 2.5cqw;
-  height: 2.5cqw;
+  top: 5cqw;
+  height: 2000px;
+  background-color: var(--ratp-beige);
+  z-index: 1;
+  pointer-events: none;
+}
+
+.stop-indicator {
+  width: 3cqw;
+  height: 3cqw;
   background-color: white;
-  border: 0.3cqw solid black;
+  border: .5cqw solid black;
   border-radius: 50%;
-  left: 1.7cqw;
-  top: .7cqw;
+  left: 1.2cqw;
+  top: 0.7cqw;
   position: relative;
-  z-index: 2;
+  z-index: 2; /* Important : au-dessus du masque ::after */
   flex-shrink: 0;
 }
 
@@ -97,7 +103,7 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
   border: none;
   width: 2.5cqw;
   height: 2.5cqw;
-  left: .9cqw;
+  left: 0.9cqw;
   top: 0cqw;
   display: flex;
   align-items: center;
@@ -127,10 +133,12 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
 }
 
 @keyframes blink {
-  0%, 49% {
+  0%,
+  49% {
     background-color: var(--ratp-yellow);
   }
-  50%, 100% {
+  50%,
+  100% {
     background-color: white;
   }
 }
@@ -153,10 +161,9 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
 .stop-transition-leave-active {
   transition: transform 1s ease-in-out;
   z-index: 10;
-  
-  position: absolute; 
-  width: 100%; /* Nécessaire car en absolute l'élément perd sa largeur implicite */
-  /* ------------------------ */
+
+  position: absolute;
+  width: 100%;
 }
 .stop-transition-enter-from {
   transform: translateY(100%);
@@ -169,11 +176,11 @@ defineProps<{ stops: StopWithTime[]; primaryColor: string }>();
 .stop-transition-move {
   transition: transform 1s ease-in-out;
   z-index: 10;
-
 }
-.stop.is-last-stop .stop-name{
-    background-color: black;
-    color:white;
-    padding: .6cqw;
+
+.stop.is-last-stop .stop-name {
+  background-color: black;
+  color: white;
+  padding: 0.6cqw;
 }
 </style>
