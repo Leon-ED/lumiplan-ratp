9b1e9595f339dba2cf5a227fdae10eec4ea94a9d
Leon-ED <edmeeleon@outlook.fr>
2025-11-27 13:37:51 +0100

    Ajout de la recherche basique de ligne et de véhicules

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,27 @@
 import { Mode } from "fs";
-import { Line } from "./types";
+import { Desserte, Line } from "./types";
 
 export class Api {
   static apiBaseUrl = "https://ecrans-api.gwadz.fr/";
+  static async getVehiclesOnLine(lineId: string): Promise<Desserte[]> {
+    const endpoint = `${this.apiBaseUrl}lines/line/${lineId}/vehicles_on_line`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch vehicles: ${response.status} ${response.statusText}`
+      );
+    }
+    try {
+      const vehiclesData: any = await response.json();
+      const dessertes: Desserte[] = vehiclesData.journeys
+      return dessertes;
+    }
+    catch (error) {
+      console.error("Error parsing vehicles data:", error);
+      return [];
+    }
+  }
+
 
   static async getLine(lineId: string): Promise<Line> {
     const endpoint = `${this.apiBaseUrl}lines/lines:${lineId}`;
@@ -16,8 +35,8 @@ export class Api {
       const linesData: any = await response.json();
       const line = linesData.lines[0];
       const realLine: Line = {
-        id: line.id,
-        name: line.name.startsWith("TER ") ? 'TER'  : line.name,
+        id: line,
+        name: line.shortName,
         color: line.backgroundColor,
         textColor: line.textColor,
         //@ts-expect-error
@@ -26,6 +45,32 @@ export class Api {
       return realLine;
     } catch (error) {
       console.error("Error parsing line data:", error);
+      throw error;
+    }
+  }
+
+  static async searchLines(query: string): Promise<Line[]> {
+    const endpoint = `${this.apiBaseUrl}lines/search/${encodeURIComponent(
+      query
+    )}`;
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to search lines: ${response.status} ${response.statusText}`
+      );
+    }
+    try {
+      const linesData: any = await response.json();
+      const lines: Line[] = linesData.lines.map((line: any) => ({
+        id: line.ref,
+        name: line.shortName,
+        color: line.backgroundColor,
+        textColor: line.textColor,
+        mode: line.mode.toUpperCase() as Mode,
+      }));
+      return lines;
+    } catch (error) {
+      console.error("Error parsing search results:", error);
       throw error;
     }
   }
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,3 +1,94 @@
 <template>
-    <h1>Salut</h1>
+  <main>
+    <input class="search-input" type="text" v-model="_search" />
+    <ol>
+      <li v-for="line in lines" :key="line.id" @click="selectedLine = line">
+        <LineLogo :line="line" class-name="line-logo" size="3em" />
+      </li>
+    </ol>
+    <section class="desserte-list" v-if="dessertes.length > 0">
+      <h2>Dessertes pour la ligne {{ selectedLine?.name }}</h2>
+      <ul>
+        <li v-for="desserte in dessertes" @click="selectedDesserte = desserte">
+          {{ desserte.direction }} - Prochain arret: {{ desserte.stops[0]?.stop.name }}
+        </li>
+      </ul>
+    </section>
+  </main>
 </template>
+<script setup lang="ts">
+import { watchDebounced } from "@vueuse/core";
+import { ref } from "vue";
+import { Desserte, Line } from "../types";
+import { Api } from "../api";
+import LineLogo from "../components/Other/LineLogo.vue";
+const selectedLine = ref<Line | null>(null);
+const selectedDesserte = ref<Desserte | null>(null);
+const dessertes = ref<Desserte[]>([]);
+const _search = ref("");
+const lines = ref<Line[]>([]);
+watchDebounced(
+  _search,
+  async () => {
+    try {
+      const apiLines = await Api.searchLines(_search.value);
+      if (!apiLines || apiLines.length === 0) {
+        return;
+      }
+      lines.value = apiLines;
+      console.log("Fetched lines:", lines.value);
+    } catch (error) {
+      console.error("Error fetching search results:", error);
+    }
+  },
+  { debounce: 300 }
+);
+watchDebounced(
+  selectedLine,
+  async (newLine) => {
+    if (newLine) {
+      try {
+        const vehicles = await Api.getVehiclesOnLine(newLine.id);
+        dessertes.value = vehicles;
+        console.log("Fetched vehicles for line:", vehicles);
+      } catch (error) {
+        console.error("Error fetching vehicles for line:", error);
+      }
+    }
+  },
+  { debounce: 300 }
+);
+</script>
+<style scoped>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 3em;
+}
+input[type="text"] {
+  font-size: 1.5em;
+  border-radius: 0.3em;
+  box-shadow: unset;
+  margin-bottom: 1em;
+  width: 40%;
+  box-sizing: border-box;
+  padding: 1vw 0.5vw;
+  border: gray 1px solid;
+}
+input[type="text"]:focus {
+  outline: none;
+}
+ol {
+  list-style: none;
+  padding: 0;
+}
+li {
+  width: fit-content;
+}
+li:hover {
+  cursor: pointer;
+  background-color: #f0f0f0;
+}
+</style>
