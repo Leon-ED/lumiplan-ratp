c2edf50a152c41ce96dd38a04975f148958c0791
Leon-ED <edmeeleon@outlook.fr>
2025-12-03 00:29:26 +0100

    Ajout basique paneau latéral des message

---
+++ src/components/SidePanel/Messages.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="message">
+    <img
+      src="../../assets/img/info.png"
+      alt="messages icon"
+      class="message-icon"
+    />
+    <br/>
+    <span class="message-text">Signalez tout comportement suspect à nos agents.</span>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
+<style lang="css" scoped>
+.message {
+  width: 100%;
+  height: 100%;
+  padding: 2cqw;
+  box-sizing: border-box;
+}
+.message-icon {
+  width: 4cqw;
+  height: 4cqw;
+  margin-right: 1cqw;
+  margin-bottom: 1cqw;
+}
+.message-text {
+  font-size: 2cqw;
+  color: #212121;
+}
+</style>
