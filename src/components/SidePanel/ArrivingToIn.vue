ec428ea4f515ed253397afa7a35f38ea799238fb
Leon-ED <edmeeleon@outlook.fr>
2025-12-03 00:29:04 +0100

    Écran prochaines arrivées : on utilise mtn un tableau au lieu d'une liste

--- src/components/SidePanel/ArrivingToIn.vue
+++ src/components/SidePanel/ArrivingToIn.vue
@@ -1,20 +1,25 @@
 <template>
   <aside class="arriving-to-in">
-    <div class="header">
-      <div class="text">Arrivée prévue dans</div>
-      <div class="unit">min</div>
-    </div>
-    <ol class="stops-list">
-      <li
-        v-for="desserte in stopsList.slice(stopsList.length - 1, 999)"
-        class="desserte"
-      >
-        <span class="stop-name">{{ desserte.stop.name }}</span>
-        <span class="minutes-before-arrival">{{
-          getMinutesFromDate(desserte.timeOfArrival)
-        }}</span>
-      </li>
-    </ol>
+    <table cellpadding="15">
+      <thead>
+        <tr>
+          <th class="destination-header">Arrivée prévue dans</th>
+          <th class="minutes-header">min</th>
+        </tr>
+      </thead>  
+      <tbody>
+        <tr
+          v-for="desserte in stopsList.slice(0, 3)"
+          :key="desserte.stop.id"
+        >
+          <td class="destination">
+            {{ desserte.stop.name }}</td>
+          <td class="minutes-before-arrival">{{
+            getMinutesFromDate(desserte.timeOfArrival)
+          }}</td>
+        </tr>
+      </tbody>
+    </table>
   </aside>
 </template>
 <script setup lang="ts">
@@ -33,39 +38,36 @@ defineProps<Props>();
   background-color: #f4efea;
   font-family: "ParisineBold";
 }
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+thead {
   padding: .5cqw 2.4cqw 0 2cqw;
   font-size: 1.3cqw;
   font-family: "ParisineRegular";
   color: #212121;
 }
-.stops-list {
-  list-style: none;
-}
-.desserte {
-  display: grid;
-  grid-template-columns: 90% 30%;
-  font-size: 1.5cqw;
-  align-items: center;
-  justify-content: space-between;
-}
+
 .minutes-before-arrival {
   color: black;
   font-size: 3cqw;
 }
-.stop-name {
+.destination {
+  font-size: 1.5cqw;
   color: var(--ratp-blue);
 }
-ol {
-  padding: 0;
-}
-li {
+
+tbody tr {
   padding: 1cqw 2cqw;
 }
-li:nth-child(odd) {
+tbody tr:nth-child(odd) {
   background-color: #dedad6;
+}
+.minutes-before-arrival {
+  text-align: center;
+}
+.destination,.destination-header {
+  text-align: left;
 }
 </style>
