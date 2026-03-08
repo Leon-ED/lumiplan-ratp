0fd0834be57b4073738850c434fef0b3a37ce885
Leon-ED <edmeeleon@outlook.fr>
2026-03-08 22:52:55 +0100

    fix line reloading

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <router-view v-slot="{ Component }">
   <keep-alive>
-    <component :is="Component" />
+    <component :key="$route.fullPath" :is="Component" />
   </keep-alive>
 </router-view>
 </template>
