6669a9956fca301b02d83c95f1c349c482ad5f47
Leon-ED <edmeeleon@outlook.fr>
2026-08-18 13:58:31 +0200

    Ajout bouton pour revneir de l'éditeur vers l'accueil

--- src/pages/Editor.vue
+++ src/pages/Editor.vue
@@ -457,6 +457,25 @@ const deleteLine = (line: Line) => {
             <span class="launch-icon">▶</span>
             Lancer l'écran
           </button>
+          <button class="btn-exit" @click="router.push('/')">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-box-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+              />
+            </svg>
+          </button>
         </div>
       </header>
       <div class="editor-grid">
@@ -551,6 +570,24 @@ const deleteLine = (line: Line) => {
 }
 .btn-launch-screen:active {
   transform: translateY(0);
+}
+.btn-exit{
+  background-color: #b3b2b2;
+  color: rgb(16, 15, 15);
+  border: none;
+  border-radius: 8px;
+  padding: 12px;
+  font-size: 1.1rem;
+  font-weight: 900;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  cursor: pointer;
+  transition: all 0.2s ease;
+}
+.btn-exit:hover {
+  background-color: #a1a1a1;
+  transform: translateY(-2px);
 }
 .launch-icon {
   font-size: 1.2rem;
